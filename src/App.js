a61bafc3262bc998fecc568c446a8dd38b01c889
Anna Burley <anna.burley@sky.uk>
2019-04-30 16:21:07 +0100

    Fixed nowtv toolkit issue

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from "react";
 import "./App.css";
-import PropTypes from "prop-types";
+// import PropTypes from "prop-types";
 
 import StaticHeader from "@nowtv/nowtv-web-toolkit/src/react/components/StaticHeader/StaticHeader.react";
 
 function App() {
   return (
     <div className="App">
-      <StaticHeader />
+      <StaticHeader colour="green" />
     </div>
   );
 }
-StaticHeader.propTypes = {
-  className: PropTypes.string,
-  colour: PropTypes.oneOf,
-  heading: PropTypes.string
-};
+// StaticHeader.propTypes = {
+//   className: PropTypes.string,
+//   colour: PropTypes.oneOf,
+//   heading: PropTypes.string
+// };
 export default App;
